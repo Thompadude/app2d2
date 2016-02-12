339e1997145bd1e41a84241a71c791cb9b7634ab
Thomas Lansing <thompa.lansing@gmail.com>
2016-02-12 11:34:40 +0100

    fix #8 fix #22

--- app/src/main/java/sw/app2d2/AboutActivity.java
+++ app/src/main/java/sw/app2d2/AboutActivity.java
@@ -1,20 +1,14 @@
 package sw.app2d2;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class AboutActivity extends MainActivity {
-
-    TextView tvAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme();
         setContentView(R.layout.activity_about);
-
-        tvAbout = (TextView) findViewById(R.id.tvAbout);
-        tvAbout.setText(R.string.about);
     }
 
 }
