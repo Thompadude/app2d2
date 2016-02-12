92175c0e29b562b2e5fe47b103449d2b1d9ee5dc
Thomas Lansing <thompa.lansing@gmail.com>
2016-02-12 12:20:17 +0100

    adjust layout in about activity

--- app/src/main/java/sw/app2d2/AboutActivity.java
+++ app/src/main/java/sw/app2d2/AboutActivity.java
@@ -1,6 +1,8 @@
 package sw.app2d2;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutActivity extends MainActivity {
 
@@ -9,6 +11,17 @@ public class AboutActivity extends MainActivity {
         super.onCreate(savedInstanceState);
         setTheme();
         setContentView(R.layout.activity_about);
+
+        TextView tvAbout = (TextView) findViewById(R.id.tvAbout);
+        ImageView ivAbout = (ImageView) findViewById(R.id.ivAbout);
+
+        // Fade in the about text in 1 second.
+        tvAbout.setAlpha(0f);
+        tvAbout.animate().alpha(1f).setDuration(1000l).setListener(null);
+
+        // Fade in the image in 3 seconds.
+        ivAbout.setAlpha(0f);
+        ivAbout.animate().alpha(1f).setDuration(3000l).setListener(null);
     }
 
 }
